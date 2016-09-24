21339271b97d9fd3e409a100496851e7cc7a1e8d
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-09-24 12:42:52 -0700

    thread user displays correctly

--- app/components/messages/thread-list.js
+++ app/components/messages/thread-list.js
@@ -10,14 +10,26 @@ var ThreadList = React.createClass({
   render: function(){
     console.log(this.props.data);
     var threadNodes = this.props.data.map(function(thread, index) {
+      var sender_first_name = thread.sender_first_name;
+      var sender_last_name = thread.sender_last_name;
+      var sender_profile_pic = thread.sender_profile_pic;
+
+      if (thread.sender_first_name === sessionStorage.first_name) {
+        console.log(thread.sender_first_name, sessionStorage.first_name);
+        sender_first_name = thread.recipient_first_name;
+        sender_last_name = thread.recipient_last_name;
+        sender_profile_pic = thread.recipient_profile_pic;
+      }
+
+
       return (
         <Thread
           key={'thread' + thread.thread_id}
           id={thread.thread_id}
           data_tag={thread.thread_id}
-          sender_first_name={thread.sender_first_name}
-          sender_last_name={thread.sender_last_name}
-          sender_profile_pic={thread.sender_profile_pic}
+          sender_first_name={sender_first_name}
+          sender_last_name={sender_last_name}
+          sender_profile_pic={sender_profile_pic}
           recipient_first_name={thread.recipient_first_name}
           recipient_last_name={thread.recipient_last_name}
           recipient_profile_pic={thread.recipient_profile_pic}
